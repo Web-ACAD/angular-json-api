535c8d24c453e09b07bed7868cf3726c2b069181
David Kudera <kudera.d@gmail.com>
2018-03-27 10:59:40 +0200

    Fix error with barrel index file

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export {JsonApiData, JsonApiResource, JsonApiResourceIdentifier, JsonApiRelationship} from './json-api-schema';
 export {WaJsonApiModule} from './json-api.module';
-export * from './normalizer';
+export * from './normalizer/index';
