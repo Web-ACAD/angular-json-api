af052898d15664d037eab08dc1c00ea2077ffca6
David Kudera <kudera.d@gmail.com>
2018-03-31 17:29:30 +0200

    Register missing service to module

--- src/json-api.module.ts
+++ src/json-api.module.ts
@@ -2,12 +2,16 @@ import {NgModule} from '@angular/core';
 
 import {WaJsonApiNormalizer} from './normalizer/index';
 import {ApiClient} from './client/index';
+import {ApiEntityMetadataLoader} from './entity/index';
+import {ApiMapper} from './mapping/index';
 
 
 @NgModule({
 	providers: [
 		WaJsonApiNormalizer,
 		ApiClient,
+		ApiEntityMetadataLoader,
+		ApiMapper,
 	],
 })
 export class WaJsonApiModule {}
