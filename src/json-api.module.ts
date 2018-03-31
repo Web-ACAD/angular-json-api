08c0128edbf8362b6d2b728a8e56d063bc5be713
David Kudera <kudera.d@gmail.com>
2018-03-31 19:40:28 +0200

    Import HttpClientModule to JsonApiModule

--- src/json-api.module.ts
+++ src/json-api.module.ts
@@ -1,4 +1,5 @@
 import {NgModule} from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
 
 import {JsonApiNormalizer} from './normalizer/index';
 import {JsonApiClient} from './client/index';
@@ -7,6 +8,9 @@ import {JsonApiMapper} from './mapping/index';
 
 
 @NgModule({
+	imports: [
+		HttpClientModule,
+	],
 	providers: [
 		JsonApiNormalizer,
 		JsonApiClient,
